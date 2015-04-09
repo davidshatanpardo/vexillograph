b99541ebb30d33908a71c0bbc0c6fafb38fa2790
davidshatanpardo <david.shatanpardo@gmail.com>
2015-04-09 01:53:01 -0400

    xf

--- js.js
+++ js.js
@@ -1492,22 +1492,22 @@ function generate() {
     if (values[7]<=-1){
         light1 = light1 + (values[3]*3) + (values[7]*3) - 10;
         console.log(light1);
-        sat1 = sat1 + (values[5]*3) + (values[7]*3) + 10;
+        sat1 = sat1 + (values[5]*3) + (values[7]*3) + 2;
         console.log(sat1);
 
         light2 = light2 + (values[3]*3) + (values[7]*3) - 10;
         console.log(light2);
-        sat2 = sat2 + (values[5]*3) + (values[7]*3) + 10;
+        sat2 = sat2 + (values[5]*3) + (values[7]*3) + 2;
         console.log(sat2);
 
         light3 = light3 + (values[3]*3) + (values[7]*3) - 10;
         console.log(light3);
-        sat3 = sat3 + (values[5]*3) + (values[7]*3) + 10;
+        sat3 = sat3 + (values[5]*3) + (values[7]*3) + 2;
         console.log(sat3);
 
         light4 = light4 + (values[3]*3) + (values[7]*3) - 10;
         console.log(light4);
-        sat4 = sat4 + (values[5]*3) + (values[7]*3) + 10;
+        sat4 = sat4 + (values[5]*3) + (values[7]*3) + 2;
         console.log(sat4);
     }
     
